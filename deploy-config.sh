89e2258e7cb5bc15ce8e4a56172b902af1ec753c
Yanbing Shi <bangbing@gmail.com>
2022-02-17 15:25:24 -0800

    add deployment script

---
+++ deploy-config.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Stop on error.
+set -e
+
+subscriptionId=$1
+resourceGroupName=$2
+nginxDeploymentName=$3
+nginxConfigurationFile=$4
+templateFile="template.json"
+
+if [ -f "$nginxConfigurationFile" ]
+then
+    echo "NGINX configuration"
+    cat "$nginxConfigurationFile"
+    echo ""
+else 
+    echo "NGINX configuration $nginxConfigurationFile does not exist."
+    exit 32
+fi
+
+encodedConfigContent=$(base64 $nginxConfigurationFile)
+echo "Base64 encoded NGINX configuration"
+echo "$encodedConfigContent"
+echo ""
+
+wget -O "$templateFile" https://raw.githubusercontent.com/bangbingsyb/azure-quickstart-templates/master/quickstarts/nginx.nginxplus/nginx-single-configuration-file/azuredeploy.json
+echo "Downloaded the ARM template for deploying NGINX configuration"
+cat "$templateFile"
+echo ""
+
+echo "Deploying NGINX configuration"
+echo "Subscription: $subscriptionId"
+echo "Resource group: $resourceGroupName"
+echo "NGINX deployment name: $nginxDeploymentName"
+echo ""
+
+az account set -s "$subscriptionId" --verbose
+az deployment group create --resource-group "$resourceGroupName" --template-file "$templateFile" --parameters nginxDeploymentName="$nginxDeploymentName" rootConfigContent="$encodedConfigContent" --verbose
